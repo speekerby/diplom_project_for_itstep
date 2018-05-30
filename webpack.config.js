d45b8b0a8d5e75ad6e9c74e76fd996329a838d94
speekerby <speekerby@live.ru>
2018-05-30 20:25:29 +0300

    Update src

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, './bundle'),
         filename: 'bundle.js',
-        publicPath: 'dist/'
+        publicPath: 'bundle/'
     },
     devServer: {
         overlay: true
