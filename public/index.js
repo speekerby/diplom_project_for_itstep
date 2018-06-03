6179e8e6b8755952904ac6062697a7eaffde5758
speekerby <speekerby@live.ru>
2018-06-03 21:00:25 +0300

    setting config webpack

--- public/index.js
+++ public/index.js
@@ -1,3 +1,11 @@
+//JQUERY
 import $ from 'jquery';
+//CSS
+import './scss/styles.scss'
+
 
 $('.title').html('JQUERY');
+var newOne = () => {
+    console.log("Hello World..!");
+}
+newOne();
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
-module.exports = {
+
+let conf = {
     entry: './public/index.js',
     output: {
         path: path.resolve(__dirname, './bundle'),
@@ -9,6 +11,31 @@ module.exports = {
     },
     devServer: {
         overlay: true
-    }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: "babel-loader"
+            }, {
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: ['css-loader', 'sass-loader']
+                })
+            }
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin("styles.css"),
+    ]
 };
 
+module.exports = (env, options) => {
+    let production = options.mode === 'production';
+
+    conf.devtool = production
+        ? 'source-map'
+        : 'eval-sourcemap';
+    return conf;
+}
